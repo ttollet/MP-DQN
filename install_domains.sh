2c6b2057f99c0023f5dda9a2225ebb8f9fe4f69c
Tollet <tollet@stori-ub1804>
2023-04-18 17:33:11 +0100

    Initial success

---
+++ install_domains.sh
@@ -0,0 +1,3 @@
+poetry run pip install -e git+https://github.com/cycraig/gym-platform#egg=gym_platform
+poetry run pip install -e git+https://github.com/cycraig/gym-goal#egg=gym_goal
+poetry run pip install -e git+https://github.com/cycraig/gym-soccer#egg=gym_soccer 
